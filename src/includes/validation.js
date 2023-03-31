e081ec3149966bd78366553f0249b3d8912eac0d
Harpreet Singh Saluja <hssaluja2508@gmail.com>
2023-04-01 01:17:03 +0530

    adds ui for validation page

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,5 +1,5 @@
 import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage, configure } from 'vee-validate'
-import { required, min, max, email, regex } from '@vee-validate/rules'
+import { required, min, max, email, regex, confirmed } from '@vee-validate/rules'
 
 export default {
     install(app) {
@@ -7,6 +7,7 @@ export default {
         app.component("VeeField", VeeField)
         app.component("ErrorMessage", ErrorMessage)
         defineRule('required', required)
+        defineRule('confirmed', confirmed)
         defineRule('min', min)
         defineRule('max', max)
         defineRule('email', email)
@@ -18,6 +19,7 @@ export default {
                     min: `The field ${ctx.field} is too short`,
                     max: `The field ${ctx.field} is too long`,
                     email: `The field ${ctx.field} must be a valid email`,
+                    confirmed: `The password fields do not match`,
                     regex: `The field ${ctx.field} must contain at least one lowercase letter, one uppercase letter, one digit and one special character`
                 }
                 const message = messages[ctx.rule.name] ? messages[ctx.rule.name] : `The field ${ctx.field} is invalid`
