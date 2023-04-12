e9f8ddac836f47ca0da76a6ab5e18183747d82e8
Harpreet Singh Saluja <hssaluja2508@gmail.com>
2023-04-12 17:32:28 +0530

    prep for deployment

--- uploadDairy.js
+++ uploadDairy.js
@@ -4,19 +4,19 @@ await setDoc(doc(db, 'featured', 'dairy'), {
     info: [
         {
             name: 'Butter',
-            img: 'src/assets/images/dairy/butter.jpg',
+            img: 'images/dairy/butter.jpg',
             price: '1.81',
             quantity: '1 Pound'
         },
         {
             name: 'Cheese',
-            img: 'src/assets/images/dairy/cheese.jpg',
+            img: 'images/dairy/cheese.jpg',
             price: '13.19',
             quantity: '1 Pound'
         },
         {
             name: 'Condensed Milk',
-            img: 'src/assets/images/dairy/condensed-milk.jpg',
+            img: 'images/dairy/condensed-milk.jpg',
             price: '43.36',
             quantity: '24 Ounce'
         }
--- uploadMeat.js
+++ uploadMeat.js
@@ -4,91 +4,91 @@ await setDoc(doc(db, 'featured', 'meat-and-poultry'), {
     info: [
         {
             name: 'Bacon',
-            img: 'src/assets/images/meat-and-poultry/bacon.jpg',
+            img: 'images/meat-and-poultry/bacon.jpg',
             price: '34.99',
             quantity: '28 Ounce'
         },
         {
             name: 'Beef',
-            img: 'src/assets/images/meat-and-poultry/beef.jpg',
+            img: 'images/meat-and-poultry/beef.jpg',
             price: '13.19',
             quantity: '2 Ounce'
         },
         {
             name: 'Chicken',
-            img: 'src/assets/images/meat-and-poultry/chicken.png',
+            img: 'images/meat-and-poultry/chicken.png',
             price: '33.36',
             quantity: '6.5 Ounce'
         },
         {
             name: 'Chicken Breasts',
-            img: 'src/assets/images/meat-and-poultry/chicken-breast.jpg',
+            img: 'images/meat-and-poultry/chicken-breast.jpg',
             price: '33.36',
             quantity: '6.5 Ounce'
         },
         {
             name: 'Duck',
-            img: 'src/assets/images/meat-and-poultry/duck.jpg',
+            img: 'images/meat-and-poultry/duck.jpg',
             price: '16.98',
             quantity: '2.5 Ounce'
         },
         {
             name: 'Ground Beef',
-            img: 'src/assets/images/meat-and-poultry/ground-beef.jpg',
+            img: 'images/meat-and-poultry/ground-beef.jpg',
             price: '40.66',
             quantity: '1.5 Ounce'
         },
         {
             name: 'Ground Pork',
-            img: 'src/assets/images/meat-and-poultry/ground-pork.jpg',
+            img: 'images/meat-and-poultry/ground-pork.jpg',
             price: '34.75',
             quantity: '3.4 Ounce'
         },
         {
             name: 'Ground Turkey',
-            img: 'src/assets/images/meat-and-poultry/ground-turkey.jpg',
+            img: 'images/meat-and-poultry/ground-turkey.jpg',
             price: '30.24',
             quantity: '2.4 Ounce'
         },
         {
             name: 'Ham',
-            img: 'src/assets/images/meat-and-poultry/ham.jpg',
+            img: 'images/meat-and-poultry/ham.jpg',
             price: '27.64',
             quantity: '3.5 Ounce'
         },
         {
             name: 'Lamb',
-            img: 'src/assets/images/meat-and-poultry/lamb.jpg',
+            img: 'images/meat-and-poultry/lamb.jpg',
             price: '35.4',
             quantity: '4.8 Ounce'
         },
         {
             name: 'Pork',
-            img: 'src/assets/images/meat-and-poultry/pork.jpg',
+            img: 'images/meat-and-poultry/pork.jpg',
             price: '19.4',
             quantity: '4.4 Ounce'
         },
         {
             name: 'Sausages',
-            img: 'src/assets/images/meat-and-poultry/sausages.jpg',
+            img: 'images/meat-and-poultry/sausages.jpg',
             price: '24.6',
             quantity: '2.4 Ounce'
         },
         {
             name: 'Steak',
-            img: 'src/assets/images/meat-and-poultry/steak.jpg',
+            img: 'images/meat-and-poultry/steak.jpg',
             price: '22.3',
             quantity: '5.4 Ounce'
         },
         {
             name: 'Turkey',
-            img: 'src/assets/images/meat-and-poultry/turkey.jpg',
+            img: 'images/meat-and-poultry/turkey.jpg',
             price: '14.2',
             quantity: '2.4 Ounce'
         },
         {
             name: 'Veal',
-            img: 'src/assets/images/meat-and-poultry/veal.jpg',
+            img: 'images/meat-and-poultry/veal.jpg',
             price: '25.8',
             quantity: '1.5 Ounce'
         },
