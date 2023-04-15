4fb6f5b02c25ba7a928000a9a9945c8daf195ed3
Harpreet Singh Saluja <hssaluja2508@gmail.com>
2023-04-15 23:05:18 +0530

    chore: adds firebase analytics

--- src/includes/firebase.config.js
+++ src/includes/firebase.config.js
@@ -1,6 +1,8 @@
-import firebase from 'firebase/compat/app'
+import { initializeApp } from "firebase/app";
 import { deleteUser, getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 import { getFirestore, getDoc, doc, setDoc } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
 
 
 const firebaseConfig = {
@@ -12,12 +14,12 @@ const firebaseConfig = {
     appId: "1:293562259434:web:4cb8038789e4befd890493",
     measurementId: "G-CEFQTM1MC5"
 };
-const app = firebase.initializeApp(firebaseConfig)
-
+// const app = firebase.initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth()
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
-
 
 export {
     auth, provider, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, db, getDoc, doc, setDoc, deleteUser, signOut
