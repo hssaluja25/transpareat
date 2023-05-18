84a4ae4904e9df2f5342c914cfef554105a0582d
Harpreet Singh Saluja <hssaluja2508@gmail.com>
2023-05-18 11:32:28 +0530

    fixes the bg image issue on about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,10 @@
 
   <main>
     <!-- Hero image in the bg -->
-    <div class="hero min-h-screen" style="background-image: url(@/assets/images/about/chef.webp)">
+    <div
+      class="hero min-h-screen"
+      :style="{ backgroundImage: 'url(src/assets/images/about/chef.webp)' }"
+    >
       <div class="hero-overlay bg-opacity-60"></div>
       <div class="hero-content text-center text-neutral-content">
         <div class="max-w-4xl">
